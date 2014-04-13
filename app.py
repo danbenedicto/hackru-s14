484ab4f2df46ae0d50f861eaa554aa9b8f199f04
Dan Benedicto <danbenedicto@gmail.com>
2014-04-13 07:07:20 -0400

    fixed

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-	return 'welcome!'
+	return render_template('index.html')
 
 @app.route('/submit', methods=['GET'])
 def submit():
 	name = request.args.get('text')
 	scale = minor_pentatonic_scale('C2') + minor_pentatonic_scale('C3') + minor_pentatonic_scale('C4') + minor_pentatonic_scale('C5') + minor_pentatonic_scale('C6') + minor_pentatonic_scale('C7')
-	m = Melopy('newsong')
+	m = Melopy('static/newsong')
 	for c in name.lower():
 		m.add_eighth_note(scale[ord(c) - 97])
 	m.render()
-	return render_template('playback.html', name='mysong')
+	return render_template('playback.html', name='song')
 	
 
 app.run(debug=True)
