8b4d242d334e6d8af870407ca7d0707021ca974a
Dan Benedicto <danbenedicto@gmail.com>
2014-04-13 07:22:31 -0400

    replace submit action; add database

--- app.py
+++ app.py
@@ -5,19 +5,29 @@ from melopy.scales import *
 
 app = Flask(__name__)
 
+db = dataset.connect('sqlite:///file.db')
+
+table = db['beeps']
+
 @app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
 
-@app.route('/submit', methods=['GET'])
+@app.route('/submit', methods=['POST'])
 def submit():
-	name = request.args.get('text')
+	name = request.form['name']
+	message = request.form['message']
+	beep = dict(name=name, message=message)
+
 	scale = minor_pentatonic_scale('C2') + minor_pentatonic_scale('C3') + minor_pentatonic_scale('C4') + minor_pentatonic_scale('C5') + minor_pentatonic_scale('C6') + minor_pentatonic_scale('C7')
-	m = Melopy('static/newsong')
-	for c in name.lower():
+	m = Melopy('static/' + name)
+	for c in message.lower():
 		m.add_eighth_note(scale[ord(c) - 97])
 	m.render()
-	return render_template('playback.html', name='song')
+	
+	table.insert(beep)
+	
+	return render_template('playback.html', name=name)
 	
 
 app.run(debug=True)
