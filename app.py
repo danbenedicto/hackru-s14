d7af6ada08c3d594b0ac120a0c21ca1d2d0d803c
Dan Benedicto <danbenedicto@gmail.com>
2014-04-12 23:01:46 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+	name = request.args.get('name')
+	return "sup nigga " + name
+
+app.run(debug=True)
